8b394c62a2e7bfb5ded20cd806380b011efc25c1
Someones <240647889@qq.com>
2020-10-23 01:00:18 +0800

    Initial commit

--- app/src/main/java/top/someones/cardmatch/ui/WorkshopActivity.java
+++ app/src/main/java/top/someones/cardmatch/ui/WorkshopActivity.java
@@ -9,22 +9,25 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import top.someones.cardmatch.R;
-import top.someones.cardmatch.core.GameManagement;
 import top.someones.cardmatch.core.ImageCache;
 import top.someones.cardmatch.entity.Mod;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
 public class WorkshopActivity extends AppCompatActivity {
-
+    private static final String DOMAIN = "http://192.168.43.50:8080/CardMatchService/";
+    private OkHttpClient httpClient;
     private RecyclerView modList;
     private ProgressDialog loading;
 
@@ -36,37 +39,62 @@ public class WorkshopActivity extends AppCompatActivity {
         modList = findViewById(R.id.modList);
         modList.setLayoutManager(new LinearLayoutManager(this));
         loading = ProgressDialog.show(this, "请稍后", "正在连接到创意工坊");
+        httpClient = new OkHttpClient();
         Intent intent = new Intent(WorkshopActivity.this, ModInfoActivity.class);
 
-        Mod[] mods = GameManagement.getMods(this);
-
-        Request request = new Request.Builder().get().url("https:someones.top").build();
-        Call call = new OkHttpClient().newCall(request);
+        Request request = new Request.Builder().get().url(DOMAIN + "Index").build();
+        Call call = httpClient.newCall(request);
         call.enqueue(new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                loading.dismiss();
                 runOnUiThread(() -> {
-                            AlertDialog.Builder builder = new AlertDialog.Builder(WorkshopActivity.this);
-                            builder.setTitle("网络错误");
-                            builder.setMessage(e.getMessage());
-                            builder.setCancelable(false);
-                            builder.setNegativeButton("返回", (dialog, which) -> WorkshopActivity.this.finish());
-                            builder.create().show();
-                        }
-                );
+                    AlertDialog.Builder builder = new AlertDialog.Builder(WorkshopActivity.this);
+                    builder.setTitle("网络错误");
+                    builder.setCancelable(false);
+                    builder.setNegativeButton("返回", (dialog, which) -> WorkshopActivity.this.finish());
+                    builder.setMessage(e.getMessage());
+                    builder.create().show();
+                });
             }
 
             @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                Log.d("net!", response.code() + "");
-                runOnUiThread(() -> {
-                    modList.setAdapter(new ModAdapter(mods, v -> {
-                        startActivity(intent.putExtra("dd", "ds"));
-                    }));
-                    loading.dismiss();
-                });
+            public void onResponse(@NotNull Call call, @NotNull Response response) {
+                loading.dismiss();
+                try {
+                    JSONObject json = new JSONObject(response.body().string());
+                    JSONArray jarr = json.getJSONArray("hot");
+                    Mod[] mods = new Mod[jarr.length()];
+                    for (int i = 0; i < jarr.length(); i++) {
+                        mods[i] = jsonToMod(jarr.getJSONObject(i));
+                    }
+                    runOnUiThread(() -> modList.setAdapter(new ModAdapter(mods, v -> startActivity(intent.putExtra("uuid", v.getUUID())))));
+                } catch (Exception e) {
+                    runOnUiThread(() -> {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(WorkshopActivity.this);
+                        builder.setTitle("错误");
+                        builder.setCancelable(false);
+                        builder.setNegativeButton("返回", (dialog, which) -> WorkshopActivity.this.finish());
+                        builder.setMessage(e.getMessage());
+                        builder.create().show();
+                    });
+                }
             }
         });
+    }
+
+    private Mod jsonToMod(JSONObject json) throws Exception {
+        String uuid = json.getString("UUID");
+        Bitmap bitmap = ImageCache.getCache(uuid);
+        if (bitmap == null) {
+            Call imageCall = httpClient.newCall(new Request.Builder().get().url(DOMAIN + "GetImgBinServlet" + uuid).build());
+            Response imageResponse = imageCall.execute();
+            bitmap = BitmapFactory.decodeStream(imageResponse.body().byteStream());
+            if (bitmap != null) {
+                ImageCache.addWorkshopCache(uuid, bitmap);
+            }
+        }
+        return new Mod(uuid, json.getString("Mod_Name"), bitmap, json.getString("Author"), json.getDouble("Version"));
     }
 
     @Override
